11ca6c47d7c9cabefbda5d27813e2bde7eef50b6
ochiaihitomi <sky.force.1013@gmail.com>
2021-01-18 05:46:34 +0000

    [Update] 4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,20 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.use_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def edit
@@ -18,6 +24,12 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
