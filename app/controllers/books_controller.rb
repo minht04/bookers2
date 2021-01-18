81a3e682fb2018b8f0489365b52bb19e08b67328
ochiaihitomi <sky.force.1013@gmail.com>
2021-01-18 04:39:10 +0000

    [Update] 4-10

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def upedate
+  end
+
+  def destroy
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_125032) do
+ActiveRecord::Schema.define(version: 2021_01_18_043358) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
